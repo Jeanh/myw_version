c3bad81650e408bf107d3c58a6b0c9b597c11dfc
monolok <antoinebertin@me.com>
2014-11-07 00:42:45 +0100

    new

--- global.js
+++ global.js
@@ -1,13 +1,14 @@
 $(document).ready(function (){
 
-$("[data-toggle='modal']").modal('toggle');
-
-
-
-
-
-
-
+$("[data-toggle='modal']").modal('click');
 
 
 });
+
+function initialize() {
+
+var input = document.getElementById('searchTextField');
+var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
