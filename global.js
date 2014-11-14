443d0f6ab9e36a4a36db5918124bc839b532bed4
monolok <antoinebertin@me.com>
2014-11-14 20:07:29 +0100

    14/11/14

--- global.js
+++ global.js
@@ -2,18 +2,35 @@ $(document).ready(function (){
 	
 	$('#drop1').click(function(){
 		$('#drop2').toggle();
+		$('#drop4').hide();
 	});
 
 	$('#drop3').click(function(){
 		$('#drop4').toggle();
+		$('#drop2').hide();
 	});
 
 	$('#filter').click(function(){
 		$('#box_filter').toggle();
 	});
 
+	$('#L').click(function() {
+		$('.language').toggle();
+		$('.language').siblings().hide();
+	});
+
+	$('#N').click(function() {
+		$('.nationality').toggle();
+		$('.nationality').siblings().hide();
+	});
+
+	$('#pointer2').click(function() {
+		$(".modal").modal('hide');
+	});
+
 	$(window).load(function() {
       $("[data-toggle='modal']").trigger('click');
+
 	});
 
 
